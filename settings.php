8a29616a4d7834b9c57276dad9bff655e8a295f0
pkhushdil <psk287@nyu.edu>
2016-04-19 19:55:50 -0400

    added radio button functionality

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@ session_start();
 	</head>
 <body>
 <h5> Privacy Settings </h5>
-<form>
+<form action="settings.php" method="Post"> 
 	<table>
 		<tr>
 			<td><input type="radio" name="Privacy" id="Public" value="Public" > Public </td>
@@ -26,3 +26,31 @@ session_start();
 
 </body>
 </html>
+<?php
+   $host        = "host=pdc-amd01.poly.edu";
+   $port        = "port=5432";
+   $dbname      = "dbname=ku336";
+   $credentials = "user=ku336 password=e0eycb7p";
+
+   $conn = pg_connect( "$host $port $dbname $credentials"  );
+   	if(!$conn)
+   		{
+    		echo "Error : Unable to open database\n";
+   		}
+   if(isset($_POST["Public"]))
+   		{
+   			$privacy=3;
+   		}
+   	elseif(isset($_POST["Friends"]))
+   		{
+   			$privacy=2;
+   		}
+   	elseif(isset($_POST["Public"]))
+   		{
+   			$privacy=1;
+   		}
+   	$userName=$_SESSION['user'];
+   	$stmt=pg_prepare($conn,"s","select sp_update_privacy($1,$2)");
+	$sqlname="s";
+	$result=pg_execute($conn,$sqlname,array($userName,$privacy));					
+?>
