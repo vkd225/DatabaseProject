a8ac700ba3273d9d3bdd5d9c73beb6707231b357
keerthan21 <keerthan9008@gmail.com>
2016-04-26 20:27:48 -0400

    comments for the diary entry...positioning to be done

--- settings.php
+++ settings.php
@@ -20,29 +20,28 @@ $privacyFOF="unchecked";
 	$result1=pg_execute($conn,$sqlname1,array("$userName"));
 	$rows1=pg_num_rows($result1);
 		   if ($rows1>0)
-		   		{	
+		   		{
 		   			$selected=pg_fetch_array($result1,0,PGSQL_NUM);
-		   			$selectedPrivacy=$selected[0];	
-		   			echo $selectedPrivacy;
-		   			if ($selectedPrivacy==3) 
+		   			$selectedPrivacy=$selected[0];
+		   			#echo $selectedPrivacy;
+		   			if ($selectedPrivacy==3)
 		   				{
 		   					$privacyPublic='checked';
-		   					
+
 		   				}
-		   			if ($selectedPrivacy==2) 
+		   			if ($selectedPrivacy==2)
 		   				{
 		   					$privacyFriend='checked';
-		   					
+
 		   				}
-		   			if ($selectedPrivacy==1) 
+		   			if ($selectedPrivacy==1)
 		   				{
 		   					$privacyFOF='checked';
-		   					
-		   				}	
+
+		   				}
 				}
 				$SQL1=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname1));
 		   pg_query($SQL1);
-			?>
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,7 +50,7 @@ $privacyFOF="unchecked";
 	</head>
 <body>
 <h5> Privacy Settings </h5>
-<form action="settings.php" method="Post"> 
+<form action="settings.php" method="Post">
 	<table>
 		<tr>
 			<td><a href="profile.php">Profile</a></td>
@@ -59,7 +58,7 @@ $privacyFOF="unchecked";
 		<tr>
 			<td><a href="friends.php">Friends</a></td>
 		</tr>
-		
+
 		<tr>
 			<td><input type="radio" name="Privacy" id="Public" value="Public" <?php echo $privacyPublic;?>> Public </td>
   		</tr>
@@ -72,7 +71,7 @@ $privacyFOF="unchecked";
   		<tr>
   			<td><button id="update" name="update" value="update">Update</button></td>
   		</tr>
-  			
+
 	</table>
 
 </form>
@@ -80,35 +79,35 @@ $privacyFOF="unchecked";
 </body>
 </html>
 <?php
-  
-   	if (isset($_POST["update"])) 
-   		{	
+
+   	if (isset($_POST["update"]))
+   		{
    			$selectedPrivacy=$_POST["Privacy"];
-			   		
+
    			if($selectedPrivacy=="Public")
-		   		{	
+		   		{
 		   			$privacyPublic='checked';
 		   			$privacy=3;
-		   			
+
 		   					   		}
 	  	 	if($selectedPrivacy=="Friends")
-		   		{	
+		   		{
 		   			$privacyFriend='checked';
 		   			$privacy=2;
 		   		}
 	   		if($selectedPrivacy=="FriendsOfFriends")
-		   		{	
+		   		{
 		   			$privacyFOF='checked';
 		   			$privacy=1;
-		   		
+
 		   		}
-   			  			
-   			
+
+
    			$stmt=pg_prepare($conn,"s","select sp_update_privacy($1,$2)");
 			$sqlname="s";
-			$result=pg_execute($conn,$sqlname,array($userName,$privacy));					
-  			
-   		}	
-   		
-   	
+			$result=pg_execute($conn,$sqlname,array($userName,$privacy));
+
+   		}
+
+
 ?>
