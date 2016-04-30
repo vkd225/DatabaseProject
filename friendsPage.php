8ac88172840f3bfeed51fb032f91bfe5c74d008b
pkhushdil <psk287@nyu.edu>
2016-04-30 01:44:40 -0400

    Keerthan check this for your profile page

--- friendsPage.php
+++ friendsPage.php
@@ -14,7 +14,7 @@ session_start();
    		}
     $userName=$_SESSION['user'];
     $friend=$_SESSION['friend'];
-		   #profile from database
+	#profile from database
 	$stmt=pg_prepare($conn,"s","select profile from user_profile where user_name=$1");
 	$sqlname="s";
 	$result=pg_execute($conn,$sqlname,array("$friend"));
@@ -31,21 +31,46 @@ session_start();
 	$SQL=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname));
 	pg_query($SQL);
 
-		   #diary entry from database
-	$stmt1=pg_prepare($conn,"s","select * from user_diary where user_name=$1");
-	$sqlname1="s";
-	$result1=pg_execute($conn,"s",array("$friend"));
-	$rows1=pg_num_rows($result1);
-	if ($rows1>0)
-		{
-			$diary=pg_fetch_array($result1,NULL,PGSQL_NUM);
+?>
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+		{	
+			$stmt8=pg_prepare($conn,"s","select * from sp_post_diary_entry($1)");
+			$sqlname8="s";
+			$result8=pg_execute($conn,"s",array("$friend"));
+
+			$rows8=pg_num_rows($result8);
+		  	if ($rows8>0)
+		   		{
+		   			while ($row=pg_fetch_array($result8,NULL,PGSQL_NUM))
+						{
+							
+							if (isset($_POST[$row[0]]))
+								{
+									
+									if (isset($_POST[$row[0]."comment"]))
+										{
+											$body1=$_POST[$row[0]."comment"];
+											$diaryentry_id=$row[0];
+											$stmt7=pg_prepare($conn,"k","select sp_insert_user_diary_comment($1,$2,$3,$4)");
+											$sqlname7="k";
+									   		$result7=pg_execute($conn,"k",array($userName,$friend,$body1,$diaryentry_id));
+									   		$SQL7=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname7));
+				  						 	pg_query($SQL7);
+		   									header("Location:friendsPage.php");
+				   						}
+				   					break;
+
+		   						}	
+		   				}
+
+		   		}
+
+
+
+			$SQL8=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname8));
+		    pg_query($SQL8);
 		}
-	else
-	    {
-		   	echo"No such record exists";
-		}
-	$SQL1=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname1));
-	pg_query($SQL1);
 ?>
 
 <!DOCTYPE>
@@ -55,9 +80,13 @@ session_start();
 	<title></title>
 </head>
 <body>
+<a href="profile.php">Profile</a>
+<a href="settings.php">Settings</a>
+<a href="friends.php">Friends</a>
+<a href="search.php">Search</a>
 <h1>Profile Page</h1>
 <a href="friends.php">Friends</a>
-<form method="post" action="profile.php">
+<form method="post" action="friendsPage.php">
 	<table>
 		<tr>
 			<td>
@@ -68,6 +97,34 @@ session_start();
 			<td>
 				<h3>Profile Comment</h3>
 			</td>
+<?php
+		$stmt2=pg_prepare($conn,"s","select * from sp_search_comments_by_commented_on($1)");
+		$sqlname2="s";
+		$result2=pg_execute($conn,"s",array("$friend"));
+		$rows2=pg_num_rows($result2);
+		if ($rows2>0)
+		    {
+		    	while ($row=pg_fetch_array($result2,NULL,PGSQL_NUM))
+					{
+?>
+						<tr>
+							<td>
+								<input type="text" name="commenter" readonly="" value="<?php echo ($row[0]);?>"></input>
+							</td>
+							<td>
+								<textarea><?php echo($row[1]);?> </textarea> 
+							</td>
+							<td>
+								<input type="text" name="time_posted_comment" readonly="" value="<?php echo ($row[2]);?>"></input>
+							</td>
+					  	</tr>
+<?php
+					}
+			}
+		$SQL2=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname2));
+		pg_query($SQL2);
+?>
+
 		</tr>
 		<tr>
 			<td>
@@ -101,78 +158,83 @@ session_start();
 		   	}
 		}
 ?>
-<?php
-		$stmt2=pg_prepare($conn,"s","select * from sp_search_comments_by_commented_on($1)");
-		$sqlname2="s";
-		$result2=pg_execute($conn,"s",array("$friend"));
-		$rows2=pg_num_rows($result2);
-		if ($rows2>0)
-		    {
-		    	while ($row=pg_fetch_array($result2,NULL,PGSQL_NUM))
-					{
-?>
-						<tr>
-							<td>
-								<input type="text" name="commenter" readonly="" value="<?php echo ($row[0]);?>"></input>
-							</td>
-							<td>
-								<textarea><?php echo($row[1]);?> </textarea> 
-							</td>
-							<td>
-								<input type="text" name="time_posted_comment" readonly="" value="<?php echo ($row[2]);?>"></input>
-							</td>
-					  	</tr>
-<?php
-					}
-			}
-		$SQL2=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname2));
-		pg_query($SQL2);
-?>
         <!--The diary entry-->
         <tr>
-			<td>
-				<h3>Diary entry</h3>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<input type="text" name="title" id="diary_title" placeholder="title" required/></input>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<input type="text" name="body" id="diary_body" placeholder="body" required/></input>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<input type="submit" name="post" value="post"></input>
-			</td>
-		</tr>
+        	<td>
+        		<h3>Diary Entries</h3>
+        	</td>
+        </tr>
 <?php
-		if($_SERVER['REQUEST_METHOD']=='POST')
-			{
-				$stmt6=pg_prepare($conn,"s","select * from sp_post_diary_entry($1)");
-				$sqlname6="s";
-			    $result6=pg_execute($conn,"s",array("$userName"));
-			    $rows6=pg_num_rows($result6);
-			    if ($rows6>0)
-			   		{
-			   			while ($row=pg_fetch_array($result6,NULL,PGSQL_NUM))
-							{
+
+	$stmt3=pg_prepare($conn,"s","select * from sp_post_diary_entry($1)");
+	$sqlname3="s";
+    $result3=pg_execute($conn,"s",array("$friend"));
+    $rows3=pg_num_rows($result3);
+    if ($rows3>0)
+	   {
+	   		while ($row3=pg_fetch_array($result3,NULL,PGSQL_NUM))
+				{
 ?>
-								<tr>
-									<td><input type="text" name="title_" readonly="" value="<?php echo ($row[0]);?>"></input></td>
-									<td><textarea><?php echo($row[1]);?> </textarea> </td>
-									<td><input type="text" name="time_posted_comment" readonly="" value="<?php echo ($row[2]);?>"></input></td>
-			  					</tr>
-<?php
+					<tr>
+						<td>
+							<input type="text" name="title_" readonly="" value="<?php echo ($row3[1]);?>"></input>
+						</td>
+						<td>
+							<textarea><?php echo($row3[2]);?> </textarea> 
+						</td>
+						<td>
+							<input type="text" name="time_posted_comment" readonly="" value="<?php echo ($row3[3]);?>"></input>
+						</td>
+					<?php
+
+						$result4=pg_query("select * from sp_show_user_diary_comment_updated($row3[0])");
+						$rows4=pg_num_rows($result4);
+					    if ($rows4>0)
+						   {
+						   		while ($row4=pg_fetch_array($result4,NULL,PGSQL_NUM))
+									{
+					?>
+										<tr>
+											<td><input type="text" name="commenter" readonly="" value="<?php echo ($row4[0]);?>"></input></td>
+											<td><textarea><?php echo($row4[1]);?> </textarea> </td>
+											<td><input type="text" name="time_posted_comment" readonly="" value="<?php echo ($row4[2]);?>"></input></td>
+						  				</tr>	
+									
+						<?php
+									}
+									
 							}
-					}
-			    $SQL6=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname6));
-			    pg_query($SQL6);
-			}
-?>
+						else
+							{
+						?>		
+								<tr>
+									<td>
+										<label>No Comments</label>>
+									</td>
+								</tr>
+						<?php
+							}	
+						?>	
+									
+				  	<tr>
+						<td>
+							<input type="text" name="<?php echo $row3[0]."comment";?>" id="diary_body_comment" placeholder="comment"/></input>
+						</td>
+						<td>
+							<input type="submit" name="<?php echo $row3[0]; ?>" value="diary_comment"></input>
+						</td>
+					</tr>
+						
+<?php
+				}
+		}
+	$SQL3=sprintf('DEALLOCATE "%s"',pg_escape_string($sqlname3));
+	pg_query($SQL3); 
+	
+	
+?> 
+
+
 </table>
 </form>
 </body>
