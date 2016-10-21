a451de5978d8b0b98240ccee88d79e0d3a5fee04
pkhushdil <psk287@nyu.edu>
2016-10-21 17:15:10 -0400

    Update edit_diary.php

--- edit_diary.php
+++ edit_diary.php
@@ -15,10 +15,10 @@ $privacyPublic="unchecked";
 	
 ?>
 <?php
-	$host        = "host=pdc-amd01.poly.edu";
-	$port        = "port=5432";
-	$dbname      = "dbname=ku336";
-	$credentials = "user=ku336 password=e0eycb7p";
+	$host        = "";
+	$port        = "";
+	$dbname      = "";
+	$credentials = "";
 
    	$conn = pg_connect( "$host $port $dbname $credentials"  );
    	if(!$conn)
