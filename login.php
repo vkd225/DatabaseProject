e783af43df5717bfdacfba00e51e615cd94d53c0
pkhushdil <psk287@nyu.edu>
2016-10-21 17:17:34 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 
 <?php
 session_start();
-  $host        = "host=pdc-amd01.poly.edu";
-  $port        = "port=5432";
-  $dbname      = "dbname=ku336";
-  $credentials = "user=ku336 password=e0eycb7p";
+  $host        = "";
+  $port        = "port=";
+  $dbname      = "dbname=";
+  $credentials = "user= password=";
   $conn = pg_connect( "$host $port $dbname $credentials"  );
     if(!$conn)
       {
