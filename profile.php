54cd7a67d5420868dfdc1a9aff348edd6eb5766a
pkhushdil <psk287@nyu.edu>
2016-10-21 17:19:52 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -13,10 +13,10 @@ $privacyPublic="unchecked";
 	$privacyFOF="unchecked";
 ?>
 <?php
-	$host        = "host=pdc-amd01.poly.edu";
-	$port        = "port=5432";
-	$dbname      = "dbname=ku336";
-	$credentials = "user=ku336 password=e0eycb7p";
+	$host        = "host=";
+	$port        = "port=";
+	$dbname      = "dbname=";
+	$credentials = "user= password=";
 
    	$conn = pg_connect( "$host $port $dbname $credentials"  );
    	if(!$conn)
